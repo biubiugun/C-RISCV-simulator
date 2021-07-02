84a7d9ed118b9e70c4adae43e9557ea5f1cedb16
biubiugun <1027800576@qq.com>
2021-07-02 16:54:04 +0800

    debug resetCmake

--- main.cpp
+++ main.cpp
@@ -1,31 +1,6 @@
 #include "CPUSimulator.h"
 using namespace std;
 
-void read(u_int8_t *mem){
-    int address;
-    std::string data;
-    while(1){
-        std::cin >> data;
-        if(std::cin.eof()){
-            return;
-        }
-        if(data[0] == '@'){
-            sscanf(data.c_str() + 1, "%x", &address);
-        }
-        else{
-            int sum;
-            sscanf(data.c_str(), "%x", &sum);
-            for(int i = 1; i <= 3; ++i){
-                int tmp_data;
-                std::cin >> std::hex >> tmp_data;
-                sum += (tmp_data << (i * 8));
-            }
-            memcpy(mem + address, &sum, sizeof(sum));
-            address += 4;
-        }
-    }
-}
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
